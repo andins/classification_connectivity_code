cb4e605a2ba876013df89b54d177f11e87a45319
andins <a.insabato@gmail.com>
2018-04-12 09:19:26 +0200

    just some notes on TODOs

---
+++ classif_benchmark.py
@@ -0,0 +1,9 @@
+# for each cond generate a binary adjacency matrix A
+
+# for each sample generate a continuous weight matrix W
+
+# generate time series with MOU.simulate(C=A*W)
+
+# estimate FC and EC from time series
+
+# predict cond from FC and EC
